f59c3bc067662f9a99dcec3cdcf79a93c63a92bb
fajar <fajardwiutomo75@gmail.com>
2025-07-31 16:27:00 +0700

    fix: deployment

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,21 +1,21 @@
 // src/types/express.d.ts
 
-import { Request } from 'express';
+import { Request } from 'express'
 
 // Define JwtPayload for your custom user claims
 export interface JwtPayload {
-  user_id: string;
-  email: string;
-  role_id: number;
-  name?: string;
-  is_active?: boolean;
-  iat?: number;
-  exp?: number;
+    user_id: string
+    email: string
+    role_id: number
+    name?: string
+    is_active?: boolean
+    iat?: number
+    exp?: number
 }
 
 // Extend Express Request
 declare module 'express-serve-static-core' {
-  interface Request {
-    user?: JwtPayload;
-  }
+    interface Request {
+        user?: JwtPayload
+    }
 }
